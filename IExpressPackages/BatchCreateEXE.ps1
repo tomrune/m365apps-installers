a54d1add929239668cb1401f88d685d4e54512db
Tom Rune Berg <tomrune@knowledgegroup.no>
2023-04-20 23:42:54 +0200

    Squashed commit of the following:
    commit 14f63be786dd61b43dc4a809cbd08347099e3277
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 23:39:53 2023 +0200
    
        Cleaned up SED file paths
    
        Removed personally identifiable path names
    
    commit 2ee542b43830e382a93e8febc93972e6f8f691db
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 23:22:36 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Another crack at the progress indicators
    
    commit d5e3fc2a4ca163522114de2da1fafe047b4d031e
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:50:39 2023 +0200
    
        Update Beta64-ProPlus.SED
    
        Fixed file mapping
    
    commit 357d9ccb515fca59ac3f3e46ba3190e8ba2afcd3
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:50:23 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Yet another progress bar fix
    
    commit 0120a68822b2a077c021f094e07705a497c21e74
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:39:01 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Tried to fix progress bars
    
    commit d94b8b8d49ed73a587f3f067877473ef99ab87cc
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:29:04 2023 +0200
    
        Disable test
    
    commit 062aa9db7a07e3e3d3ab5ef86fa03eddd88494ed
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:28:46 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Fixed progress indicators and added comments
    
    commit 7f1bdceee6c3033ff4a117b2fa4c63b8f4eb7e48
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:11:12 2023 +0200
    
        Corrected SED file typo
    
    commit 141109c4252df17cc3923d47b6b65e3fd998974b
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:10:28 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Added Wait-parameter
    
    commit ab1e5f32334ed4a2fb9f877797cb1d78ba31c92b
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 22:06:19 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Added progressbars and cleanup actions
    
    commit b52245b655a3894383bc9878cebcc0cf7112f4dc
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 21:39:30 2023 +0200
    
        Update BatchCreateEXE.ps1
    
        Updated to write full paths into the SED Files relative to the computer that runs the script
    
    commit 5d7aaf1558cf81f386c7fb412ec1ef275c6348b2
    Author: Tom Rune Berg <tomrune@knowledgegroup.no>
    Date:   Thu Apr 20 21:23:05 2023 +0200
    
        Test

--- IExpressPackages/BatchCreateEXE.ps1
+++ IExpressPackages/BatchCreateEXE.ps1
@@ -1,6 +1,71 @@
-$SEDFiler = Get-ChildItem C:\users\tomrune\GitHub\trb-m365apps-installers\IExpressPackages\*.SED
+## Script that generates EXE files for Office
+# The script handles the limitation of IExpress not working properly with local folders 
 
-foreach($sed in $SEDFiler) {
-    iexpress.exe /N $sed
+# Health check - do we have PSINI installed?
+Write-Progress -Activity "Build EXE files" -Status "Determines if PSINI is installed" -Id 1 -PercentComplete 5
+if((Get-Module PSINI) -eq $null) {Write-Error "Missing PSINI - Please install it"; break}
 
+# If health check passed, we can cleanup old temp files
+Remove-Item $PSScriptRoot\*.CAB -Force
+Remove-Item $PSScriptRoot\*.DDF -Force
+Remove-Item $PSScriptRoot\*.INF -Force
+# Remove old EXE Files
+Remove-Item $PSScriptRoot\*.EXE -Force
+
+# Resolves local enviroment paths
+Write-Progress -Activity "Build EXE files" -Status "Determines local paths" -Id 1 -PercentComplete 10
+$SEDFilesToProcess = Get-ChildItem "$PSScriptRoot\*.SED" # All the SED-files in the folder
+$SEDFolder = (Resolve-Path "$PSScriptRoot").Path # Path to this folder
+$ODTFolder = (Resolve-Path "$PSScriptRoot\..\ODT").Path # Path to ODT folder for setup.exe
+$ODTConfigFolder = (Resolve-Path "$PSScriptRoot\..\ODT-Configurations").Path # Path to ODT Configs for XML file (folder name must match SED file name)
+
+# Give some feedback on the way
+Write-Progress -Activity "Build EXE files" -Status "Updates SED Files with proper paths" -Id 1 -PercentComplete 25
+$SEDprogress = 1
+$EXEProgress = 0
+
+# Let's update the SED files with what we know
+foreach($sedFile in $SEDFilesToProcess) {
+    # Show where we are
+    $FilesToProcess = $SEDFilesToProcess.Count
+    $sedName = $sedFile.BaseName # This is the name of the SED file without file extention
+    Write-Host "Processing $sedName.sed - $SEDprogress / $FilesToProcess"
+
+    # Prepare SED for processing
+    $sedConfigFolder = "$ODTConfigFolder\$sedName" # Name of folder containing XML file
+    $tempSEDObject = Get-IniContent $sedFile # Read the SED file into an object for manipulation
+    # Build an array where we set the paths
+    $newSedPaths = @{
+        'SourceFiles0' = "$ODTFolder\"
+        'SourceFiles1' = "$SEDFolder\"
+        'SourceFiles2' = "$sedConfigFolder\"
+    }
+    # Write the updated settings into the SED file
+    Set-IniContent -InputObject $tempSEDObject -NameValuePairs $newSedPaths -Sections "SourceFiles" | Out-IniFile -FilePath $sedFile -Force
+    
+    # Progress to next
+    $SEDprogress++
+    if($CurrentProgress -gt 50) {Write-Progress -Activity "Build EXE files" -Status "Updates SED Files with proper paths" -Id 1 -PercentComplete 40}
 }
+
+## Let's make those EXE files now that the SED Files point to the correct folders
+# We run Iexpress in automatic build mode so you can see the progress.
+Write-Progress -Activity "Build EXE files" -Status "Compiling EXE files" -Id 1 -PercentComplete 50
+foreach($sedFile in $SEDFilesToProcess) {
+    # Show where we are
+    $sedName = $sedFile.BaseName # Name of SED file without extension
+    $CurrentProgress = [Math]::Round((($EXEprogress *100) / $SEDFilesToProcess.Count))
+    Write-Progress -Activity "Compiling EXE files $CurrentProgress %" -Status "Compiling $sedName" -Id 2 -ParentId 1 -PercentComplete $CurrentProgress
+        
+    # Make sure that we compile only one SED at the time to avoid race conditions and to make it easier to track errors
+    Start-Process -Filepath "$env:windir\system32\iexpress.exe" -ArgumentList "/N $sedFile" -Wait
+
+    # Update progress indicator
+    $EXEprogress++
+    if($CurrentProgress -gt 50) {Write-Progress -Activity "Build EXE files" -Status "Compiling EXE files" -Id 1 -PercentComplete 75}
+}
+# Cleanup those progress bars
+Write-Progress -Activity "Compiling EXE files" -Status "Done" -Id 2 -ParentId 1 -Completed
+Write-Progress -Activity "Build EXE files" -Status "Compiling EXE files" -Id 1 -Completed
+
+# END
